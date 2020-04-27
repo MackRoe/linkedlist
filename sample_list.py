539ad82c60f2f0f8ca95ec30c1990cf8414976c8
MackRoe <elaine.music@students.makeschool.com>
2020-04-27 13:28:55 -0700

    add sample_list.py for article

---
+++ sample_list.py
@@ -0,0 +1,11 @@
+''' Code example for Technical Article Assignment'''
+
+# create List
+dance_moves = []
+
+# add items to List
+new_move = input('Enter the URL of the new move: ')
+if new_move not None:
+    our_list.append(new_move)
+else:
+    break
